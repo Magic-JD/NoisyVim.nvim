7a93048442e37edad147c46c46b74afa75b6ba0e
MagicJoe <mag1ctr1cks@hotmail.com>
2024-07-12 00:08:05 +0700

    Improving the key listerner, adding a way to turn it on and off

--- plugin/noisy.lua
+++
@@ -1,7 +0,0 @@
-local function logKeyPress(key)
-    if(vim.api.nvim_get_mode()["mode"] == 'i' and string.len(key) == 1) then
-        vim.notify('The last key was ' .. key);
-    end
-end
-
-vim.on_key(logKeyPress);
